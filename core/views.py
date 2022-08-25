ca9bf1497fc4d7fcc0cd5b44bb0606e4d6432c50
maria <m.arialauravianaa@gmail.com>
2022-08-25 15:01:42 -0300

    Aula 7

--- core/views.py
+++ core/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
-from core.serializers import CategoriaSerializer
+from core.models import Categoria, Marca
+from core.serializers import CategoriaSerializer, MarcaSerializer
 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
 
 
--- garagem/urls.py
+++ garagem/urls.py
@@ -3,11 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, MarcaViewSet
 
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'marcas', MarcaViewSet)
 
 
 urlpatterns = [
