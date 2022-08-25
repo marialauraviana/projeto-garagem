42aa98395062641e0e196c9d72856f8b4438e58d
maria <m.arialauravianaa@gmail.com>
2022-08-25 14:56:21 -0300

    Aula 6

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+
