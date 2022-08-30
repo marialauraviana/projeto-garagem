bdeb4feb47dd76dccb56c89973ffa6c0735d7dfb
Maria Laura Viana <marialauraviana@hotmail.com>
2022-08-30 16:01:00 -0300

    Final (30/08)

--- garagem/settings.py
+++ garagem/settings.py
@@ -118,5 +118,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
